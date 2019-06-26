e90c68306f8b46f04e71eb154d0da0f1b07c72da
Kat Maldon <katharinamaldon@gmail.com>
2019-06-26 16:59:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+  get '/hello' do 
+    erb :hello
+  end
+
+  get '/goodbye' do 
+    erb :goodbye
+  end
+
+  get '/date' do 
+    erb :date
+  end
 
 end
